97e8d69c26df5d93f374288fa09a3f56a83bb50c
JAILSON COSTA GARRIDO <jailsongarrido23@gmail.com>
2023-10-24 02:34:16 +0000

    ex.js segunda feira

---
+++ js/verificador.js
@@ -0,0 +1,8 @@
+let roupa1 = 35;
+let roupa2 = 20;
+
+compras = roupa1 + roupa2
+
+let orçamento = compras <= 60
+
+console.log(orçamento)
---
+++ js/verificador2.js
@@ -0,0 +1,8 @@
+let idadeDaThays = 53;
+let idadeDoDougas = 49;
+
+let comparacao1 = idadeDaThays > idadeDoDougas;
+let comparacao2 = idadeDoDougas < idadeDaThays;
+
+console.log(comparacao1)
+console.log(comparacao2)
