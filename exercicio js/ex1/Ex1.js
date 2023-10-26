9b0c5e61f4441dc32c0bea47b1e58cea2ba34ea2
JAILSON COSTA GARRIDO <jailsongarrido23@gmail.com>
2023-10-26 02:47:47 +0000

    projeto 25/10/2023

---
+++ exercicio js/ex1/Ex1.js
@@ -0,0 +1,16 @@
+/*
+1- Faça um programa que peça dois números e verifique (usando if e else) e imprima o maior deles  
+*/
+ let numero1 = 100;
+ let numero2 = 2;
+  
+ function numeromaior(){
+    if (numero1 > numero2) {
+        console.log("o numero " + numero1 + " é maior");
+    }
+    else {
+        console.log("o numero" + numero2 + "é maior")
+    }
+ }
+
+ numeromaior()
---
+++ exercicio js/ex2/ex2.js
@@ -0,0 +1,3 @@
+/*Faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo */
+
+var numero1 = 10;
