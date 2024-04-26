b3cf6094a808b4c496c9e6eadc78c9ee27a406bd
JAILSON COSTA GARRIDO <jailsongarrido23@gmail.com>
2024-04-25 21:39:55 -0300

    Delete js directory

--- js/verificador2.js
+++
@@ -1,8 +0,0 @@
-let idadeDaThays = 53;
-let idadeDoDougas = 49;
-
-let comparacao1 = idadeDaThays > idadeDoDougas;
-let comparacao2 = idadeDoDougas < idadeDaThays;
-
-console.log(comparacao1)
-console.log(comparacao2)
